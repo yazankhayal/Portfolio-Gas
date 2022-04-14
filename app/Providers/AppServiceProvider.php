fd0c597e5cc6e4b8026cd41af6fa9e3f524303f2
Yazan Khayal <yazan.khayal@gmail.com>
2022-04-15 00:17:03 +0100

    HTTPS.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
         view()->composer('*', function ($view) {
 
